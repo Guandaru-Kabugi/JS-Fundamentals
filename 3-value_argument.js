9d3f9be05e043323252fca4279761ceaed2eb712
Guandaru-Kabugi <kabugi96@gmail.com>
2025-05-24 20:43:58 +0300

    task 3

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,6 @@
-const args = process.argv.slice(2);
-if (args.length === 0) {
+const args = process.argv.slice(2); //we cut the first two elements of the array, which are the path to the node executable and the path to the script file
+if (!args[0]) {
   console.log("No argument");
-}else if (args.length === 1) {
+}else if (!args[1]) {
   console.log(args[0]);
 }
