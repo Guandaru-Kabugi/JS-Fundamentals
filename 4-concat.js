b82f9582a61d20f76016c07a46efbd5ad984a7f0
Guandaru-Kabugi <kabugi96@gmail.com>
2025-05-25 07:54:09 +0300

    improved on the task

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,10 @@
 const args = process.argv.slice(2); //we cut the first two elements of the array, which are the path to the node executable and the path to the script file
 //we print two arguments concatenated with is
 if (!args[0]) {
-  console.log("No argument");
+  console.log("Undefined is Undefined");
 }
 else if (!args[1]) {
-  console.log(args[0]);
+  console.log(args[0] + " is Undefined");
 } else {
     console.log(args[0] + " is " + args[1]);
     }
